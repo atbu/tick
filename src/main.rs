a75d8473f5d724a68820fb796cc048948e8f4b49
Alex <186313861+atbu@users.noreply.github.com>
2025-06-21 21:10:47 +0100

    Test out using serde_json to manipulate JSON

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
+use serde::{Deserialize, Serialize};
 use std::env;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+#[derive(Debug, Deserialize, Serialize)]
+struct Task {
+    name: String,
+    completed: bool
+}
 
+fn main() {
+    let test_task = r#"
+        {
+            "name": "Test Task",
+            "completed": false
+        }
+    "#;
+
+    let task: Task = serde_json::from_str(test_task).unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() == 1 {
+        println!("Please specify a command.");
+        std::process::exit(0);
+    }
+    
     // First argument should be disregarded - this is the path to the binary.
     // So take second argument as the command the user has chosen.
     let command: &String = &args[1];
-    
     if command == "ping" {
         println!("pong");
+    } else if command == "test" {
+        println!("{:?}", task);
     }
 }
