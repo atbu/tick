7326813e69f26161dcdea5110b216233957f6615
Alex <186313861+atbu@users.noreply.github.com>
2025-06-21 21:15:55 +0100

    Use match statement rather than ifs

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,19 @@ fn main() {
     let task: Task = serde_json::from_str(test_task).unwrap();
 
     let args: Vec<String> = env::args().collect();
-    
+
     if args.len() == 1 {
         println!("Please specify a command.");
         std::process::exit(0);
     }
-    
+
     // First argument should be disregarded - this is the path to the binary.
     // So take second argument as the command the user has chosen.
     let command: &String = &args[1];
-    if command == "ping" {
-        println!("pong");
-    } else if command == "test" {
-        println!("{:?}", task);
+
+    match command.as_str() {
+        "ping" => println!("pong"),
+        "test" => println!("{:?}", task),
+        _ => println!("That is not a valid command.")
     }
 }
